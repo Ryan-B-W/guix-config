96f5c64b6e803883f79be47ca6b8765e93fa5775
Ryan "Brody" Wilcox <ryanbw@sdf.org>
2026-09-14 01:05:31 -0700

    Add machines.scm for adams

---
+++ adams-machines.scm
@@ -0,0 +1,8 @@
+(list (build-machine
+       (name "asimov.home.ryanbw.org")
+       (systems (list "x86_64-linux" "i686-linux"))
+       (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+AiNOLM8OoGTw/9qFJhk7CKr3ycsAjtAh5qbMcWmgr")
+       (user "brody")
+       (private-key "/root/.ssh/guix-build-asimov-ed25519")
+       (overload-threshold 0.999)
+       (speed 2.0)))
